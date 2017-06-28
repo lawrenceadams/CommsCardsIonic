92047fa2a4da77c35ce68af3fbeacc9f316206c6
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-28 11:55:42 +0100

    Added Sort Select Menu
    Much better than old implementation

--- src/pages/study-flash-cards/study-flash-cards.ts
+++ src/pages/study-flash-cards/study-flash-cards.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { StudyBySystemPage } from "./study-by-system/study-by-system";
-import { StudyByTypePage } from "./study-by-type/study-by-type";
-import { StudyByYearPage } from "./study-by-year/study-by-year";
-
 @IonicPage()
 @Component({
   selector: 'page-study-flash-cards',
@@ -12,15 +8,17 @@ import { StudyByYearPage } from "./study-by-year/study-by-year";
 })
 export class StudyFlashCardsPage {
 
-  studyBySystemPg = StudyBySystemPage;
-  studyByTypePg = StudyByTypePage;
-  studyByYearPg = StudyByYearPage;
+  sortCardsBy: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+    // Set default sort option to year
+    // Other options: system, type.
+    this.sortCardsBy = "Year";
     console.log('ionViewDidLoad StudyFlashCardsPage');
   }
+
 
 }
