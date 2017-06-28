57402c583bd66f5c0e2b0dab67d37148a6f058ec
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-28 12:19:29 +0100

    Added basis of cards selector

--- src/pages/study-flash-cards/study-flash-cards.ts
+++ src/pages/study-flash-cards/study-flash-cards.ts
@@ -9,6 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class StudyFlashCardsPage {
 
   sortCardsBy: string;
+  selectedSuboption: string;
+
+  sortByOptions: string[] = ["Year", "System", "Type"];
+  sortBySuboptions: string[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -18,6 +22,30 @@ export class StudyFlashCardsPage {
     // Other options: system, type.
     this.sortCardsBy = "Year";
     console.log('ionViewDidLoad StudyFlashCardsPage');
+    this.updateSelectSuboptions(this.sortCardsBy);
+  }
+
+  onSortOptionChange(selectedValue: string) {
+    console.log(selectedValue + " chosen.");
+    this.updateSelectSuboptions(selectedValue);
+  }
+
+  updateSelectSuboptions(queryselector: string) {
+    // Manipulate sortBySuboptions to give user options to choose what cards.
+    // TODO Build a better way of selecting options...
+    if (queryselector === "Year") {
+      this.sortBySuboptions = ["Year 1", "Year 2", "Year 3"];
+    } else if (queryselector === "System") {
+      this.sortBySuboptions = ["Principles", "Respiratory", "etc..."];
+    } else if (queryselector === "Type") {
+      this.sortBySuboptions = ["Breaking Badnews", "Blah", "Blah"];
+    } else {
+      console.error("Something has gone badly wrong. This should not be possible.");
+    }
+  }
+
+  onSuboptionSelect(queryselector: string) {
+    console.log(queryselector + " selected.");
   }
 
 
