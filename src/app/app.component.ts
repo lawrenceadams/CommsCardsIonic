6517782cb35689bc80c5d9532cd0680ef454c8ee
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-30 11:52:48 +0100

    Added EULA, Fixed platform detection for status bar colour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
 
-      if (cordova.platformId == 'android') {
+      if (platform.is('android')) {
         statusBar.backgroundColorByHexString('#1c3cb0');
       }
 
