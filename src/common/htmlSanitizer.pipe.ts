02d24a5ee488b56e0901103288d991a73a4ed013
Lawrence Adams <lawrencetadams@googlemail.com>
2017-07-28 10:25:44 +0100

    Reorganised /common/ to include all .store.ts's

--- src/common/htmlSanitizer.pipe.ts
+++ src/common/htmlSanitizer.pipe.ts
@@ -1,6 +1,10 @@
 import { Pipe, PipeTransform } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 
+/**
+ * The safeHTML Pipe bypasses security features built into Angular and allows HTML 
+ * to be outputted properly
+ */
 @Pipe({ name: 'safeHTML' })
 export class SafeHTMLPipe implements PipeTransform {
     constructor(private sanitizer: DomSanitizer) { }
