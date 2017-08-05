5eec808bd330e14f23641d27b818c77e214d7b35
Lawrence Adams <lawrencetadams@googlemail.com>
2017-08-05 16:54:20 +0100

    Added ability to search through YoutubeVideos

--- src/pages/useful-links/youtube-links/youtube-links.ts
+++ src/pages/useful-links/youtube-links/youtube-links.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl } from "@angular/forms";
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import 'rxjs/add/operator/debounceTime';
 
 import { YoutubeVideoPlayer } from "@ionic-native/youtube-video-player";
 
@@ -15,9 +17,32 @@ import { AnalyticsServiceProvider } from "../../../providers/analytics-service/a
 })
 export class YoutubeLinksPage {
 
+  searchTerm: string = '';
+  searchControl: FormControl;
+  items: any;
+  searching: any = false;
+
   public videos: YouTubeVideo[] = YOUTUBEVIDEOS;
 
   constructor(private analytics: AnalyticsServiceProvider, public navCtrl: NavController, public navParams: NavParams, private youtube: YoutubeVideoPlayer) {
+    this.searchControl = new FormControl();
+  }
+
+  ionViewDidLoad() {
+    this.setFilteredItems();
+
+    this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+      this.searching = false;
+      this.setFilteredItems();
+    });
+  }
+
+  setFilteredItems() {
+    this.items = this.filterItems(this.searchTerm);
+  }
+
+  onSearchInput() {
+    this.searching = true;
   }
 
   openLink(item) {
@@ -26,6 +51,14 @@ export class YoutubeLinksPage {
 
     this.analytics.YouTubeView(item.id);
     // TODO Analytics DONE TESTING NEEDED.
+  }
+
+
+  // FILTER FUNCTION
+  private filterItems(searchTerm) {
+    return this.videos.filter((video) => {
+      return video.title.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    });
   }
 
 }
