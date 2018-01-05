97bd7297a75ae9985a80c4c23db0bfb0f9654f61
Unknown <lawrencetadams@googlemail.com>
2018-01-05 13:07:00 +0000

    Implement get local/remote db
    Need to compare versions next

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from "../pages/tabs/tabs";
 import { UpdateServiceProvider } from "../providers/update-service/update-service";
+import { DatabaseServiceProvider } from "../providers/database-service/database-service";
+import { IVersionData, VersionDataModel } from "../providers/models/version.model";
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +14,10 @@ import { UpdateServiceProvider } from "../providers/update-service/update-servic
 export class MyApp {
   rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, updateSrvc: UpdateServiceProvider) {
+  localDB: IVersionData;
+  remoteDB: IVersionData;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, updateSrvc: UpdateServiceProvider, dbSrvc: DatabaseServiceProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -24,7 +29,36 @@ export class MyApp {
 
       splashScreen.hide();
 
-      updateSrvc.fetchManifest();
+
+      // Temporary trigger. Need to figure out way to setup storage properly.
+      // First of all get local database
+      // If non-existsent then initialise with empty seed.
+
+      var getLocalPromise = dbSrvc.getLocalDBVersion().then(res => { //TODO Compare local with remote
+        console.log("getLocalDBVersion() called.")
+        console.log(res)
+        this.localDB = res;
+      }).catch(err => {
+        if (err.code === 2) {
+          console.log("Initialising local db.");
+          dbSrvc.initLocalDB().then(res => { console.log("Initialised local db. + " + res) })
+        }
+      });
+
+      // Next, get remote manifest.
+      var getRemotePromise = updateSrvc.getManifest().then(res => {
+        console.log("getManifest() returned.")
+        console.log(res)
+        this.remoteDB = res;
+      });
+
+      Promise.all([getLocalPromise, getRemotePromise]).then(res => {
+        console.log("All async tasks complete.");
+        console.log("");
+        console.log("local.cards.version: " + this.localDB.cards);
+        console.log("remote.cards.version: " + this.remoteDB.cards);
+      })
+
 
     });
   }
